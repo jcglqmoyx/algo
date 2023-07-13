a6b719f704cff1f56537d48f9c4d860e1346fbf6
Chintsai Hwo <jcglqmoyx@gmail.com>
2023-07-13 11:53:17 +0800

    update

--- oj/leetcode/algorithms/1-100/51-60/60/60.cpp
+++ oj/leetcode/algorithms/1-100/51-60/60/60.cpp
@@ -5,11 +5,13 @@ using namespace std;
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        int f[10];
+        f[0] = 1;
+        for (int i = 1; i < 10; i++) f[i] = f[i - 1] * i;
         string res;
         int st = 0;
         for (int i = 1, cur = 0; i <= n; i++) {
-            int t = 1;
-            for (int j = i + 1; j <= n; j++) t *= (n - j + 1);
+            int t = f[n - i];
             int cnt = 0, d = 0;
             for (int j = 1; j <= n; j++) {
                 if ((st | (1 << j)) == st) continue;
